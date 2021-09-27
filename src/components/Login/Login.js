40d0e2bebaaf14a62bd8f636185f2ec7ba3618fd
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-27 22:16:22 +0900

    Set: SCSS 값 수정
    간단하게 수정

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
@@ -9,12 +8,12 @@ const Login = () => {
         <div className='input-background'>
           <input type='text' className='input-tag' placeholder='ID' />
         </div>
-        <div className='password input-background'>
+        <div className='password input-background br'>
           <input type='password' className='input-tag' placeholder='PASSWORD' />
         </div>
-        <br/>
-        <Link to='/signup' className='signUp'>아직 회원가입을 하지 않으셨나요?</Link> <br/>
-        <button type='submit' className='login-button'>Log in</button>
+        <br />
+        <a href='/signup' className='signUp'>아직 회원가입을 하지 않으셨나요?</a> <br/>
+        <button type='submit' className='login-button'>Log In</button>
       </form>
     </div>
   );
