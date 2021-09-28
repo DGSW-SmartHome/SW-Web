1af60ff0ef85fc1e697bc4e9c260608d8f09b6d5
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-28 16:39:15 +0900

    Add: Route signup page
    회원 가입 페이지로 가는 Route를 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route } from 'react-router';
 
 import Main from './components/Main/Main';
 import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <>
       <Route exact path='/' component={Main} />
       <Route path='/login' component={Login} />
+      <Route path='/signup' component={SignUp} />
     </>
   );
 }
