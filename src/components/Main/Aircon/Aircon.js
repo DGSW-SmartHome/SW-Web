a50e77bb505b0e04e95d73e20fb08967c7359b48
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-29 16:08:26 +0900

    Add: Aircon Control
    에어컨 바람 세기 설정 기능 추가

--- src/components/Main/Aircon/Aircon.js
+++ src/components/Main/Aircon/Aircon.js
@@ -1,32 +1,64 @@
 import { useEffect, useState } from 'react';
+import { imgData } from './AirconImg';
 import './Aircon.scss';
+import airconWind0 from '../../../Image/airconPage/airWind0.png';
 
 const Aircon = () => {
   const [airconTemp, setAirconTemp] = useState(() => JSON.parse(window.localStorage.getItem('airconTemp')) ? Number(window.localStorage.getItem('airconTemp').replace(/"/gi, '')) : 24);
+  const [windStrange, setWindStrange] = useState(1);
+
   useEffect(() => {
     window.localStorage.setItem('airconTemp', JSON.stringify(airconTemp));
   }, [airconTemp])
 
   const tempUp = () => {
     setAirconTemp(airconTemp => airconTemp + 1);
-    
   }
 
   const tempDown = () => {
     setAirconTemp(airconTemp => airconTemp - 1);
   }
 
+  const changeWindStrange = () => {
+    if (windStrange === 5) {
+      console.log('windStrange: 5');
+      setWindStrange(1);
+    } else {
+      console.log(windStrange);
+      setWindStrange(windStrange => windStrange + 1);
+    }
+  }
+
   return (
     <div className='aircon-content'>
       <span className='aircon-content-title'>희망<br/>온도</span>
       <div className='aircon-temp'>{ airconTemp }℃</div>
+        {
+          imgData.map(({ key, img }) => {
+            return (
+              <>
+                <button 
+                  onClick={() => changeWindStrange()} 
+                  className='aircon-wind-strangth'
+                >
+                  바람<br/>세기
+                </button>
+                
+                <>
+                  {windStrange === key ? (
+                    <img src={img} key={key} alt='Aircon-Img' className='wind-img' />
+                  ) : null}
+                </>
+              </>
+            )
+          })
+        }
+      <button onClick={() => setWindStrange(1)} className='aircon-onoff'>운전<br/>정지</button>
       <div className='aircon-temp-control'>
         <button className='temp-up temp-control' onClick={tempUp}>up</button> <br />
         <span>온도</span> <br />
         <button className='temp-down temp-control' onClick={tempDown}>down</button>
       </div>
-      <button className='aircon-onoff'>운전<br/>정지</button>
-      <button className='aircon-wind-strangth'>바람<br/>세기</button>
     </div>
   );
 };
