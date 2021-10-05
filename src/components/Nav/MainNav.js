1cf82c8fa8c8fb01ef7bdacc6f1bbdecebf766ce
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-10-05 22:12:20 +0900

    Add: Main Page Nav, Lock Page & More Page Nav

--- src/components/Nav/MainNav.js
+++ src/components/Nav/MainNav.js
@@ -1,29 +1,49 @@
 import HomeIcon from '../../Image/houseIcon.svg';
+import Lock from '../../Image/lock.png';
+import Setting from '../../Image/set.png';
 
 const Navbar = () => {
-  const home = {
-    position: 'absolute',
-    height: '5vh',
-    fontSize: '4vh',
-    color: 'white',
-    left: '2vw',
-    top: '4vh',
-    alignContent: 'center',
-    fontFamily: 'Jua'
-  }
-
-  const homeIcon = {
-    marginBottom: '10px',
-    verticalAlign: 'middle',
-    height: '3.5vh'
-  }
-
   return (
     <div style={home}>
       <img style={homeIcon} src={HomeIcon} alt="집 아이콘" />
       <span>집</span>
+      <img style={lockIcon} src={Lock} alt='보안' />
+      <img style={settingIcon} src={Setting} alt='설정' />
     </div>
   );
 };
+
+const home = {
+  position: 'absolute',
+  height: '5vh',
+  fontSize: '4vh',
+  color: 'white',
+  left: '2vw',
+  top: '4vh',
+  alignContent: 'center',
+  fontFamily: 'Jua'
+}
+
+const homeIcon = {
+  marginBottom: '10px',
+  verticalAlign: 'middle',
+  height: '3.5vh'
+}
+
+const lockIcon = {
+  position: 'relative',
+  marginBottom: '10px',
+  verticalAlign: 'middle',
+  height: '3.5vh',
+  left: '87.5vw'
+}
+
+const settingIcon = {
+  position: 'relative',
+  marginBottom: '10px',
+  verticalAlign: 'middle',
+  height: '3.5vh',
+  left: '88.5vw'
+}
 
 export default Navbar;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,44 @@
+import lockIcon from '../../Image/lock.png';
+import settingIcon from '../../Image/set.png';
+import homeIcon from '../../Image/houseIcon.svg';
+
+const Nav = ({ iconName, name }) => {  
+  return (
+    <div style={home}>
+      {
+        iconName === '자물쇠' ? <img style={icon} src={lockIcon} alt='lockIcon' /> : <img style={icon} src={settingIcon} alt='settingIcon' />
+      }
+      <span style={title}>{name}</span>
+      <img style={main} src={homeIcon} alt='메인 화면 아이콘' />
+    </div>
+  );
+};
+
+const home = {
+  position: 'absolute',
+  height: '5vh',
+  fontSize: '4vh',
+  color: 'whtie',
+  left: '2vw',
+  top: '4vh',
+  alignContent: 'center',
+  fontFamily: 'Jua'
+}
+
+const icon = {
+  marginBottom: '12px',
+  verticalAlign: 'middle',
+  height: '3.5vh'
+}
+
+const title = {
+  marginLeft: '0.5vw'
+}
+
+const main = {
+  position: 'relative',
+  left: '88.5vw',
+  height: '3.5vh'
+}
+
+export default Nav;
