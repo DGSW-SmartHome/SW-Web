1ba9fa25a56ff0edd6abd1fb02b2672c10d60130
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-10-19 23:35:28 +0900

    ✨ Add: API Config File

---
+++ src/API/Weather/TranslationConfig.ts
@@ -0,0 +1,10 @@
+export const baseURL = '';
+
+export const headers = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-Naver-Client-Id": "T3yvCB0htTbo0J2Z6t0a",
+    "X-Naver-Client-Secret": "ub46AjK7kd",
+    "Access-Control-Allow-Origin": "*"
+  }
+}
