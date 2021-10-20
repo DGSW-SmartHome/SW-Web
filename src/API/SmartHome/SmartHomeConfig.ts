4e74235aedf96b921c78fa5ff270312489802b85
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-10-20 20:56:32 +0900

    ✨ Add: Smart Home API Header

--- src/API/SmartHome/SmartHomeConfig.ts
+++ src/API/SmartHome/SmartHomeConfig.ts
@@ -1,7 +1,16 @@
+const GetUserToken = sessionStorage.getItem('token');
+
 export const baseURL = 'http://15.165.77.151:8000';
 
 export const headers = {
   headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+}
+
+export const UserHeaders = {
+  headers: {
+    "Authoriation": `Token${GetUserToken}`,
     "Content-Type": "application/x-www-form-urlencoded"
   }
 }
