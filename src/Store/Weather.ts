65d8eedf76a33424ddb53e744561823b0617e20d
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-11-05 15:07:45 +0900

    Fix: Recoil Duplicate atom key

--- src/Store/Weather.ts
+++ src/Store/Weather.ts
@@ -1,21 +1,21 @@
 import { atom } from "recoil";
 
 export const WeatherArea = atom<string>({
-  key: 'WeatherArea',
+  key: 'weatherArea',
   default: '지역이 입력되지 않았습니다.'
 });
 
 export const WeatherTempState = atom<number | string>({
-  key: 'WeatherTemp',
+  key: 'weatherTemp',
   default: 1000
 });
 
 export const WeatherState = atom<string | number>({
-  key: 'WeatherState',
+  key: 'weatherState',
   default: 1
 })
 
 export const WeatherImg = atom<string>({
-  key: 'WeatherImg',
+  key: 'weatherImg',
   default: '',
 })
