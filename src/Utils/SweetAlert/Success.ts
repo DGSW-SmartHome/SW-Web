d09dffe3274aab83b1b7785e4d725c0489d9ff6e
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-11-06 17:34:29 +0900

    Add: Custom SweetAlert

---
+++ src/Utils/SweetAlert/Success.ts
@@ -0,0 +1,15 @@
+import Toast from "./Toast"
+
+export const SwalChangeName = () => {
+  Toast.fire({
+    icon: 'success',
+    title: '방 이름이 변경되었습니다.'
+  })
+}
+
+export const SwalIDCheck = () => {
+  Toast.fire({
+    icon: 'success',
+    title: '사용 가능한 아이디입니다.'
+  })
+}
