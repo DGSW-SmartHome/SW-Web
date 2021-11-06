c32f863f39bd48a018c5f7fd07dcb93440d03fd3
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-11-07 04:59:31 +0900

    Add: Plug, Light toggle 기능 추가

--- src/components/Main/Electricity/ElectricityItem.tsx
+++ src/components/Main/Electricity/ElectricityItem.tsx
@@ -1,7 +1,10 @@
 import PlugON from '../../../assets/Image/MainPage/electriccityPage/plug_ON.png';
 import PlugOFF from '../../../assets/Image/MainPage/electriccityPage/plug_OFF.png';
 
+import axios from 'axios';
+
 import { res } from "src/types/Roomlist.type";
+import { UserHeaders } from 'src/api/SmartHome/SmartHome.config';
 
 import {
   ElectricityItemContainer,
@@ -9,11 +12,31 @@ import {
   ElectricityRoomName
 } from './Electricity.style';
 
+import { 
+  SwalBadRequest, 
+  SwalServerError, 
+  SwalUnauthorized 
+} from 'src/Utils/SweetAlert/Error';
+
 const ElectricityItem = ({ roomlist }:{roomlist:res}) => {
-  const { name, status } = roomlist;
+  const { id, name, status } = roomlist;
+
+  const PostTurnLight = async () => {
+    const data = new URLSearchParams();
+    data.append('id', id);
+
+    await axios.post('/v1/user/data/room/plug/', data, UserHeaders)
+    .then((res) => {
+      
+    }).catch((error) => {
+      if (error.response.status === 400) SwalBadRequest();
+      else if (error.response.status === 401) SwalUnauthorized();
+      else if (error.response.status >= 500) SwalServerError();
+    })
+  }
 
   return (
-    <ElectricityItemContainer>
+    <ElectricityItemContainer onClick={PostTurnLight}>
       {
         status === 'ON' 
         ? <ElectricityItemImage src={PlugON} alt='plugON' /> 
--- src/components/Main/Light/LightItem.tsx
+++ src/components/Main/Light/LightItem.tsx
@@ -1,7 +1,10 @@
 import lightON from '../../../assets/Image/MainPage/lightPage/light_ON.png';
 import lightOFF from '../../../assets/Image/MainPage/lightPage/light_OFF.png';
 
+import axios from 'axios';
+
 import { res } from "src/types/Roomlist.type";
+import { UserHeaders } from 'src/api/SmartHome/SmartHome.config';
 
 import { 
   LightItemContainer,
@@ -9,11 +12,31 @@ import {
   LightRoomName
 } from './Light.style';
 
+import {
+  SwalBadRequest,
+  SwalServerError,
+  SwalUnauthorized
+} from 'src/Utils/SweetAlert/Error';
+
 const LightItem = ({ roomlist }:{roomlist:res}) => {
-  const { name, status } = roomlist;
+  const { id, name, status } = roomlist;
+
+  const PostTurnPlug = async () => {
+    const data = new URLSearchParams();
+    data.append('id', id);
+
+    await axios.post('/v1/user/data/room/plug', data, UserHeaders)
+    .then((res) => {
+
+    }).catch((error) => {
+      if (error.response.status === 400) SwalBadRequest();
+      else if (error.response.status === 401) SwalUnauthorized();
+      else if (error.response.status >= 500) SwalServerError();
+    })
+  }
 
   return (
-    <LightItemContainer>
+    <LightItemContainer onClick={PostTurnPlug}>
       {
         status === 'ON' 
         ? <LightItemImage src={lightON} alt='lightON' /> 
